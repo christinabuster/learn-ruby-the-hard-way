61f3caf9d158ad3bed0f548090eef61ee8b1f9f9
christina <christinasirena@gmail.com>
2022-07-01 16:26:19 -0700

    add comments to explain floating point for equation that is techinically incorrect

--- ex3.rb
+++ ex3.rb
@@ -7,7 +7,10 @@ puts "Hens", 25 + 30 / 6
 # 100 by 3 to get 97
 puts "Roosters", 100 - 25 * 3 % 4
 puts "Now I will count the eggs:"
-#   
+# the answer prints 7 but the actual answer is 6.75, because 
+#there is no designated float point in the equation ruby rounds 
+#the value up to the 7 if you add .00 to every number it will return 
+#the true value
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 puts "Is it true that 3 + 2 < 5 - 7?"
 # is 3 + 2 which equals 5 less than 5 - 7 which is -2
