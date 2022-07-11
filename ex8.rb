a96ad546811bb8e43bee0e93f766ee590a75436a
christina <christinasirena@gmail.com>
2022-07-11 14:51:51 -0700

    add first variable and puts statement variation

--- ex8.rb
+++ ex8.rb
@@ -1,5 +1,7 @@
 # printing printing and more printing
 
 # create variable formatter equal to %s format sequence
+formatter = "%s %s %s %s"
 
-# create puts to return formatter variable
+# create puts to return formatter variable with the value of 1, 2, 3, 4
+puts formatter % [1, 2, 3, 4]
