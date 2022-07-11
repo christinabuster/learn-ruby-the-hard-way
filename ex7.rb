9201cb62e97b4e8b3fe3d3795b79f868a192ad90
christina <christinasirena@gmail.com>
2022-07-11 14:42:39 -0700

    add comments for extra credit

--- ex7.rb
+++ ex7.rb
@@ -1,10 +1,16 @@
 # more practice with strings, formating sequences, and printing, building patterns and habits
 
+#this is a string
 puts "Mary had a little lamb."
+# this is a string that uses formating sequences to inject the 
+# string 'snow' into the entire string 
 puts " its fleece was white as %s." % 'snow'
+# this is a string
 puts "and everywhere that mary went."
+# create a string . and print is 10 times
 puts "." * 10 # what'd that do?
 
+# create variables for letters to spell cheeseburger 
 end1 = "C"
 end2 = "h"
 end3 = "e"
@@ -18,5 +24,6 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
+# use the print function to print each letter consecutivly so that it spells out the work cheeseburger
 print end1 + end2 + end3 + end4 + end5 + end6 
-print end7 + end8 + end9 + end10 + end11 + end12 
+print end7 + end8 + end9 + end10 + end11 + end12
