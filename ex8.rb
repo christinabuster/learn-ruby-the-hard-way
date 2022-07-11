931bfedbe677aa4357e3929f2e0159817dad10e0
christina <christinasirena@gmail.com>
2022-07-11 15:01:37 -0700

    complete excercise 8 with comments

--- ex8.rb
+++ ex8.rb
@@ -1,8 +1,18 @@
 # printing printing and more printing
 
-# create variable formatter equal to %s format sequence
+# create variable formatter equal to %s format sequence that is empty
 formatter = "%s %s %s %s"
 
-# create puts to return formatter variable with the value of 1, 2, 3, 4
+# create puts to return formatter variable with the value of 1, 2, 3, 4 integers
 puts formatter % [1, 2, 3, 4]
+# create puts to return formatter variable with the value of number strings
 puts formatter % ["one", "two", "three", "four"]
+# create puts to return formatter variable with the value of true/false booleans
+puts formatter % [ true, false, false, true]
+# create puts to return the variable formatter 4 times which returns %s 16 times
+puts formatter % [formatter, formatter, formatter, formatter]
+# create puts to return formatter variable and assign each %s a different string 'sentence'""
+puts formatter % [ "I had this thing.",
+    "That you could type up right.", 
+    "But it didn't sing.", 
+    "so I said goodnight."]
