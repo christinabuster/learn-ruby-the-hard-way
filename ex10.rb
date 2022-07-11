e89df15144db23bb3287512151af0cfdb997d1c4
christina <christinasirena@gmail.com>
2022-07-11 16:15:48 -0700

    exercise 10 extra credit

--- ex10.rb
+++ ex10.rb
@@ -8,6 +8,7 @@
 
 #\t creates a tab indentation when returing the string
 tabby_cat = "\tI'm tabbed in."
+vertical_indent = "\vI'm tabbed /bvertically and \shave a space"
 # \n splits the string in two at the \n and puts the second half on another line
 persian_cat = "I'm split\non a line."
 # seperate each word with a \
@@ -21,9 +22,29 @@ I'll do a list:
 \t* Catnip\n\t* Grass
 MY_HEREDOC
 
+puts "The beginning"
 puts tabby_cat
+puts vertical_indent
 puts persian_cat
 puts backslash_cat
 puts fat_cat
 
 # other escape sequences: 
+# Backslash    Description
+# \a           Bell or alert
+# \b           Backspace
+# \cx          Control-x
+# \C-x         Control-x
+# \e           Escape
+# \f           Formfeed
+# \M-\C-x      Meta-Control-x
+# \n           Newline
+# \nnn         Octal notation, where n is in the range 0-7
+# \r           Carriage return
+# \s           Space
+# \t           Tab
+# \v           Vertical tab
+# \x           Character x
+# \xnn         Hexadecimal notation, where n is in the range 0-9, a-f, or A-F
+
+ 
