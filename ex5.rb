e1a58fee158867ed32fa06382197bc57f598b93a
christina <christinasirena@gmail.com>
2022-07-11 13:14:11 -0700

    Merge branch 'main' of https://github.com/christinabuster/learn-ruby-the-hard-way

--- ex5.rb
+++ ex5.rb
@@ -1,21 +1,17 @@
 name = 'Zed A. Shaw'
-age = 35 #not a lie
-height = 74 #inches
-weight = 180 #ibs
+age = 35 
+height = 74 #height in inches
+height_cm = height * 2.54 #height in centimenters
+weight = 180 #weight in ibs
+weight_kg = weight / 2.2046 #weight in kg
 eyes = 'blue'
 teeth = 'white'
 hair = 'brown'
 
-#our conversion calculator
-inches = 10
-centimeters = inches * 2.54
-
-puts " If I multiply %f.00, inches by centimeters %f.00, inches I get %f.00 centimenter" % [inches * centimeters]
-
-puts "the beginning of the excersize 5"
+puts "the beginning of the excersize 5 with extra credit"
 puts "lets talk about %s." % name
-puts "hes %d inches tall" % height
-puts "hes %d pounds heavy" % weight
+puts "hes %d inches tall or %d centimeters tall" % [height, height_cm]
+puts "he weighs %d pounds or %d kilograms " % [weight, weight_kg]
 puts "actually thats not too heavy"
 puts "hes got %s eyes and %s hair" % [eyes, hair]
 puts "his teeth are usually %s depending on the coffee" % teeth
