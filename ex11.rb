75430552262a28a242dad47e27924280458ecc68
christina <christinasirena@gmail.com>
2022-07-11 22:56:05 -0700

    extra credit add gets and chomp method with hello message

--- ex11.rb
+++ ex11.rb
@@ -6,3 +6,17 @@ print "How much do you weigh?"
 weight = gets.chomp ()
 
 puts "So, you're #{age} years old, #{height} inches tall and #{weight} pounds."
+
+#gets and chomp methods
+# use gets method to read user input, call gets and the program starts waiting for you to type then press enter
+# the result is a returned string of the content that the user typed in. You can assign this string to a variable that 
+# can be used 
+
+#example
+print "what is your name?"
+name = gets.chomp
+
+puts "Hello #{name}, nice to meet you!"
+
+#other ways to use get.chomp
+
